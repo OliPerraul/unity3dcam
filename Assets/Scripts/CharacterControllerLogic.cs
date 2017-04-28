3fc97a002ca723b10bae4e86bf8bd399668ec38e
oliperraul <superolivierperrault@gmail.com>
2017-04-28 14:50:22 -0400

    Last tut

--- Assets/Scripts/CharacterControllerLogic.cs
+++ Assets/Scripts/CharacterControllerLogic.cs
@@ -2,35 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterControllerLogic : MonoBehaviour {
-
-    [SerializeField]
-    private Animator animator;
+public class CharacterControllerLogic : MonoBehaviour
+{
     [SerializeField]
     private float directionDampTime = .25f; //delay direction
     [SerializeField]
     private float directionSpeed = 3.0f;// how fast u want the character to turn
 
+    private float speedDampTime = .05f;
+
     //ref to game camera
     [SerializeField]
-   private  ThirdPersonCamera gamecam;
+    private ThirdPersonCamera gamecam;
 
-    //private variable only, not in inspector
-    private float speed = 0.0f;
+    //movement vars
+    public float speed;
     private float direction = 0.0f;
+    private float char_angle = 0.0f;
+
     private float horizontal = 0.0f;
     private float vertical = 0.0f;
     [SerializeField]
     private float rotationDegreePerSecond = 120f; //how many sec it takes for char to rotate in full 360 if holding r /l
 
     //Animator state machine vars
+    [SerializeField]
+    private Animator animator;
+
     private AnimatorStateInfo stateInfo;
-    //hashes
-    private int m_LocomotionId = 0;
+    private AnimatorTransitionInfo transInfo;
 
- 
+    public float LocomotionThreshold { get { return 0.2f; } }
+    private int m_LocomotionId = 0; 
 
-
+    private int m_LocomotionPivotLId = 0;
+    private int m_LocomotionPivotRId = 0;
+    private int m_LocomotionPivotLTransId = 0;
+    private int m_LocomotionPivotRTransId = 0;
 
     // Use this for initialization
     void Start()
@@ -38,28 +46,52 @@ public class CharacterControllerLogic : MonoBehaviour {
         animator = GetComponent<Animator>();
         m_LocomotionId = Animator.StringToHash("Locomotion");
 
+        m_LocomotionPivotLId = Animator.StringToHash("LocomotionPivotLeft");
+        m_LocomotionPivotRId = Animator.StringToHash("LocomotionPivotRight");
+        m_LocomotionPivotLTransId = Animator.StringToHash("Locomotion -> LocomotionPivotLeft");
+        m_LocomotionPivotRTransId = Animator.StringToHash("Locomotion -> LocomotionPivotRight");
+
+        speed = 0.0f;//init speed
+
         //todo: look what it does
-        if (animator.layerCount >= 2) ;
+        if (animator.layerCount >= 2);
         {
             animator.SetLayerWeight(1, 1);
         }
-        
-        	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         stateInfo = animator.GetCurrentAnimatorStateInfo(0); //set state info in each update
 
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
-        stickToWorldSpace(this.transform, gamecam.transform, ref direction, ref speed); //pseudo returning two values
+        char_angle = 0f;
+        direction = 0f;
 
-        animator.SetFloat("Speed", speed);
+        stickToCamSpace(this.transform, gamecam.transform, ref direction, ref speed, ref char_angle, isInPivot()); //pseudo returning two values
+
+        animator.SetFloat("Speed", speed, speedDampTime, Time.deltaTime);
         animator.SetFloat("Direction", direction, directionDampTime, Time.deltaTime);
-        
+
+        if (speed > LocomotionThreshold)    // Dead zone
+        {
+            if (!isInPivot())
+            {
+                animator.SetFloat("Angle", char_angle);
+            }
+        }
+        if (speed < LocomotionThreshold && Mathf.Abs(horizontal) < 0.05f)    // Dead zone
+        {
+            animator.SetFloat("Direction", 0f);
+            animator.SetFloat("Angle", 0f);
+        }
+
+
     }
 
     // Update is called once per frame
@@ -68,57 +100,66 @@ public class CharacterControllerLogic : MonoBehaviour {
         stateInfo = animator.GetCurrentAnimatorStateInfo(0);
 
         //Debug.Log("dir "+ direction+ "hor " + horizontal);
-        
 
         //rotatin char model
-        if (isInLocomotion() && ((direction >= 0 && horizontal >= 0) || ( direction < 0 && horizontal < 0)))
+        if (isInLocomotion() && ((direction >= 0 && horizontal >= 0) || (direction < 0 && horizontal < 0)))
         {
-            Debug.Log("happy");
-
             //rot around y axis based on stick hor movement (rotate faster if stick is pushed all the way down)
             Vector3 rotationAmount = Vector3.Lerp(Vector3.zero, new Vector3(0f, rotationDegreePerSecond * (horizontal < 0f ? -1f : 1f), 0f), Mathf.Abs(horizontal));
             Quaternion deltaRotation = Quaternion.Euler(rotationAmount * Time.deltaTime); //turns eulerangle vec to quaternion and achieve frame rate indep
             this.transform.rotation = (this.transform.rotation * deltaRotation);
-        } 
+        }
+
+    }
+
+
+    public void stickToCamSpace(Transform root, Transform camera, ref float directionOut, ref float speedOut, ref float angleOut, bool isInPivot)
+    {
+        Vector3 rootDirection = root.forward;
+        Vector3 stickDirection = new Vector3(horizontal, 0, vertical);
+
+        speedOut = stickDirection.sqrMagnitude;
+
+        //get camera dir
+        Vector3 cameraDirection = camera.forward;
+        cameraDirection.y = 0.0f; //kill y value
+
+        //angle angle the camera is rotated proportional to the global forward vector (its initial dir)
+        Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, cameraDirection);
+
+        //convert joystick input to worldspace coordinate
+        Vector3 moveDirection = referentialShift * stickDirection;
+
+        //detects if stick is to the left or right of character's forward vector using the cross product (left-hand rule)
+        Vector3 axisSign = Vector3.Cross(moveDirection, rootDirection);
+
+    
+        float angleRootToMove = Vector3.Angle(rootDirection, moveDirection) * (axisSign == Vector3.zero ? 0 : -Mathf.Sign(axisSign.y));
+
+        if(!isInPivot)
+        angleOut = angleRootToMove; //retrieve angle val
+
+        angleRootToMove /= 180f; //normalize the angle value ( to have it between 1 and -1)
+
+
+        directionOut = angleRootToMove * directionSpeed; //haviing a speed to our direction (will make turn faster)
 
     }
 
 
     public bool isInLocomotion()
     {
-        
+
         return stateInfo.shortNameHash == m_LocomotionId;//query against against the state machine and look for a hash id
     }
 
 
-
-    public void stickToWorldSpace(Transform root, Transform camera, ref float directionOut, ref float speedOut)
+    public bool isInPivot()
     {
-        Vector3 rootDirection = root.forward;
-        Vector3 stickDirection = new Vector3(horizontal, 0, vertical);
-         
-        speedOut = stickDirection.sqrMagnitude;
-
-        //get camera dir
-        Vector3 cameraDirection = camera.forward;
-
-        cameraDirection.y = 0.0f; //kill y value
-
-        //angle angle the camera is rotated proportional to the global forward vector (its initial dir)
-        Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, cameraDirection);
-        //convert joystick input to worldspace coordinate
-        Vector3 moveDirection = referentialShift * stickDirection;
-        //detects if stick is to the left or right of character's forward vector using the cross product (left-hand rule)
-        Vector3 axisSign = Vector3.Cross(moveDirection, rootDirection);
-
-        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
-        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
-       
-        float angleRootToMove = Vector3.Angle(rootDirection, moveDirection)* ((axisSign.y >= 0) ? -1f : 1f);
-        angleRootToMove /= 180f; //normalize the angle value ( to have it between 1 and -1)
-        
-        directionOut = angleRootToMove * directionSpeed; //haviing a speed to our direction (will make turn faster)
-
+        return stateInfo.shortNameHash == m_LocomotionPivotLId ||
+            stateInfo.shortNameHash == m_LocomotionPivotRId ||
+            transInfo.nameHash == m_LocomotionPivotLTransId ||
+            transInfo.nameHash == m_LocomotionPivotRTransId;
     }
 
 
--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -9,55 +9,166 @@ public class ThirdPersonCamera : MonoBehaviour
     private float distanceAway; //fixed distance from the back of the character where we want to position the camera
     [SerializeField]
     private float distanceUp; //fixed distance up from the character to where we want to set the cam
+
     [SerializeField]
     private Transform followXForm; //transform of a marker for the character
+     
     [SerializeField] ///a way to look at the top of the char instead of lookin at its feet
     private Vector3 height_offset; // height from the top of the char that we want to look at
-       
+
+
+    //smooth damp vars
+    //
     [SerializeField]
     private float camSmoothDampTime = 0.1f;
     private Vector3 velocityCamSmooth = Vector3.zero;
 
-    private Vector3 lookDir; //dir well be lookin in (to calculate fixed dist from the player no matter what way he is facing)
+    [SerializeField]
+    private float lookDirDampTime = 0.1f;
+    private Vector3 lookDirVelocity = Vector3.zero;
+    //
+    
+    [SerializeField]
+    private GameObject beta;
+    private CharacterControllerLogic char_logic; //ref to character logic
+
+    private Vector3 curr_lookDir;//dir were currentyl looking into
+    private Vector3 lookDir; //direction we want to look into
     private Vector3 targetPosition;
+
+    //inputs
+    private float horizontal = 0.0f;
+    private float vertical = 0.0f;
+
+    //default cam state
+    private CamStates camState = CamStates.BEHIND;
+
+    //camera states
+    public enum CamStates
+    {
+        BEHIND, FIRSTPERSON, TARGET, FREE
+    }
+
 
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         followXForm = GameObject.FindWithTag("Player").transform;
+        char_logic = beta.GetComponent<CharacterControllerLogic>();
+
+        //init cam movement vars
+        curr_lookDir = followXForm.forward;
+        
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+        //get inputs
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
     void LateUpdate()
     {
         Vector3 characterOffset = followXForm.position + height_offset; //distance from char follow transform + some height
 
-        //calc dir from camera to player, kill y and normalize to give a valid direction with unit magnitude
-        lookDir = characterOffset - this.transform.position;
-        lookDir.y = 0; //kill y val
-        lookDir.Normalize();
-        Debug.DrawRay(this.transform.position, lookDir * distanceAway, Color.green); //draw ray from cam
+        //determine cam state
+        if (Input.GetAxis("Target") > 0.01)
+        {
+            camState = CamStates.TARGET;
 
-        targetPosition = characterOffset - lookDir*distanceAway;
-        smoothPosition(transform.position, targetPosition);//smooth transition between current pos and target pos
-  
+        }
+        else
+        {
+            camState = CamStates.BEHIND;
+
+        }
+        
+        switch (camState)
+        {
+            //case CamStates.BEHIND:
+            //    calc dir from camera to player, kill y and normalize to give a valid direction with unit magnitude
+            //    lookDir = characterOffset - this.transform.position;
+            //    lookDir.y = 0; //kill y val
+            //    lookDir.Normalize();
+
+            //    targetPosition = characterOffset - lookDir * distanceAway;
+            //    break;
+
+            case CamStates.BEHIND:
+
+                if (char_logic.speed > char_logic.LocomotionThreshold && char_logic.isInLocomotion() && !char_logic.isInPivot())
+                {
+                    //http://chortle.ccsu.edu/vectorlessons/vch09/vch09_6.html the range of the dot product of 2 unit vector is between 0 and 1
+                    lookDir = Vector3.Lerp(followXForm.right * (horizontal == 0 ? 0 : -Mathf.Sign(horizontal)), followXForm.forward * (vertical == 0 ? 0 : Mathf.Sign(vertical)), Mathf.Abs(Vector3.Dot(this.transform.forward, followXForm.forward)));
+
+                    // Calculate direction from camera to player, kill Y, and normalize to give a valid direction with unit magnitude
+                    curr_lookDir = Vector3.Normalize(characterOffset - this.transform.position);
+                    curr_lookDir.y = 0;
+
+                    // Damping makes it so we don't update targetPosition while pivoting; camera shouldn't rotate around player
+                    curr_lookDir = Vector3.SmoothDamp(curr_lookDir, lookDir, ref lookDirVelocity, lookDirDampTime);
+                }
+
+                targetPosition = characterOffset + followXForm.up * distanceUp - Vector3.Normalize(curr_lookDir) * distanceAway;
+
+                break;
+
+
+            case CamStates.TARGET:
+
+                //calc dir from camera to player, kill y and normalize to give a valid direction with unit magnitude
+                lookDir = followXForm.forward;
+                lookDir.y = 0; //kill y val
+                lookDir.Normalize();
+
+                targetPosition = characterOffset - lookDir * distanceAway;
+
+                break;
+
+        }
+            
+        //do regardless of state
+        //compensateForWalls(characterOffset, ref targetPosition);
+        smoothPosition(transform.position, targetPosition);//smooth
         //make sure cam faces character
         transform.LookAt(followXForm);
 
     }
 
+
+
+    /// <summary>  
+    /// making a smooth transition between cam curr pos and pos it wants to be in
+    /// </summary>  
     void smoothPosition(Vector3 fromPos, Vector3 toPos)
     {
         //making a smooth transition between cam curr pos and pos it wants to be in
         this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
     }
-    
-    
+
+    /// <summary>  
+    ///  Collide with walls to prevent clipping
+    /// </summary>  
+    void compensateForWalls(Vector3 fromObject, ref Vector3 toTarget)
+    {
+        Debug.DrawLine(fromObject, toTarget, Color.cyan);
+        //compensate for walls behind cam
+        RaycastHit wallHit = new RaycastHit();
+
+        // Returns true if there is any collider intersecting the line between start and end.
+        if (Physics.Linecast(fromObject, toTarget, out wallHit))
+        {
+          //  Debug.DrawRay(wallHit.point, Vector3.left, Color.red);
+
+            //change the targetPos if needed to the impact point in world space where the ray hit the collider.
+            toTarget = new Vector3(wallHit.point.x, wallHit.point.y, wallHit.point.z);
+        }
+
+    }
+
+
 
 }
